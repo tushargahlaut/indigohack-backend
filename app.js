410c3a80bbf258e345cf6f3dd67ba5367218629e
Tushar Gahlaut <tushargahlaut74@gmail.com>
2023-06-11 13:16:17 +0530

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT,
